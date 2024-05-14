b4e6cfd2011406c8e1d2f3c9794fc2bfd521a0ec
Kurt <daseeka@email.cz>
2024-05-14 19:43:13 +0200

    HiddenAttribute added

---
+++ Ogle/Attributes/HiddenAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Ogle
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class HiddenAttribute : Attribute
+    {
+    }
+}
+
--- Ogle/Controllers/OgleController.cs
+++ Ogle/Controllers/OgleController.cs
@@ -339,20 +339,24 @@ namespace Ogle
                                                              as DisplayNameAttribute)?.DisplayName ?? i.Name)
                                                .ToArray();
                 var valueProps = _settings.CurrentValue.MetricsType.GetProperties()
-                                          .Where(i => !keyProps.Contains(i.Name))
+                                          .Where(i => !keyProps.Contains(i.Name) &&
+                                                      i.GetCustomAttribute(typeof(HiddenAttribute)) == null)
                                           .Select(i => i.Name)
                                           .ToArray();
                 var valueTypes = _settings.CurrentValue.MetricsType.GetProperties()
-                                          .Where(i => !keyProps.Contains(i.Name))
+                                          .Where(i => !keyProps.Contains(i.Name) &&
+                                                      i.GetCustomAttribute(typeof(HiddenAttribute)) == null)
                                           .Select(i => i.PropertyType.Name)
                                           .ToArray();
                 var valueDisplayNames = _settings.CurrentValue.MetricsType.GetProperties()
-                                                 .Where(i => !keyProps.Contains(i.Name))
+                                                 .Where(i => !keyProps.Contains(i.Name) &&
+                                                             i.GetCustomAttribute(typeof(HiddenAttribute)) == null)
                                                  .Select(i => (i.GetCustomAttribute(typeof(DisplayNameAttribute))
                                                                as DisplayNameAttribute)?.DisplayName ?? i.Name)
                                                  .ToArray();
                 var aggregationFunc = _settings.CurrentValue.MetricsType.GetProperties()
-                                               .Where(i => !keyProps.Contains(i.Name))
+                                               .Where(i => !keyProps.Contains(i.Name) &&
+                                                           i.GetCustomAttribute(typeof(HiddenAttribute)) == null)
                                                .Select(i => i.GetCustomAttribute(typeof(AggregateAttribute)) as AggregateAttribute)
                                                .Select(i => i != null ? i.AggregationOperation.ToString().LowerCaseFirstCharacter() : "sum")
                                                .ToArray();
